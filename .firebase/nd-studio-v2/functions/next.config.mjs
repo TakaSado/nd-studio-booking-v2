c5cf44fbcc575db99971577024fe1da9dcacdfdc
TakaSado <takashi.sadohara@foru.co.jp>
2025-09-10 10:19:55 +0900

    chore(functions): set main to lib/index.js and prebuild on deploy

---
+++ .firebase/nd-studio-v2/functions/next.config.mjs
@@ -0,0 +1,11 @@
+// next.config.mjs
+var nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    typedRoutes: true
+  }
+};
+var next_config_default = nextConfig;
+export {
+  next_config_default as default
+};
